34eab78582fd4458ce531abc962d0de3f3fcbb14
etojama <torohkiss@gmail.com>
2021-12-21 14:15:32 -0800

    added checkout functionality

--- script.js
+++ script.js
@@ -5,6 +5,11 @@ const modalRight = document.querySelector("#mod-right");
 const addItem = document.querySelector("#add");
 const minusItem = document.querySelector("#minus");
 const cartSpan = document.querySelector("#cart-span");
+const result = document.querySelector("#result");
+const figure = document.querySelector("#figure");
+const trash = document.querySelector("#trash");
+const empty = document.querySelector("#empty");
+const filled = document.querySelector("#filled");
 
 let count = 1;
 
@@ -15,11 +20,36 @@ hamburger.addEventListener("click", () => {
 
 addItem.addEventListener("click", () => {
   cartSpan.textContent = count++;
+
+  figure.textContent = count - 1;
+
+  let calc = count * 125;
+  result.textContent = calc - 125;
+  trash.style.fill = "red";
 });
 
 minusItem.addEventListener("click", () => {
   cartSpan.textContent = count--;
-  if (count <= 0) {
+  figure.textContent = count + 1;
+  let calc = count * 125;
+  result.textContent = calc + 125;
+
+  if (count < 1) {
     count = 0;
   }
+  if (count === 0) {
+    trash.style.fill = "#aaa";
+  }
+});
+
+// checkout.addEventListener("click", () => {
+//   checkout.textContent = "clicked";
+// });
+
+trash.addEventListener("click", () => {
+  filled.style.opacity = 0;
+  empty.style.display = "inline";
+  empty.style.position = "absolute";
+  empty.style.top = "9rem";
+  empty.style.left = "5rem";
 });
